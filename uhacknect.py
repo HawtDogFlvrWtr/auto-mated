375199018d08fc82c8be3dc77d4733882c757fbc
root <root@uHacknect.(none)>
2015-04-28 21:29:33 +0000

    changed python script to use pyserial vs pybluez. A connection will already be made with /dev/rfcomm0, so we don't need to establish one.

--- uhacknect.py
+++ uhacknect.py
@@ -2,14 +2,18 @@
 A simple Python script to send messages to an ODBLink LX using PyBluez (with Python 2).
 """
  
-import bluetooth
+import serial
 import os
 
-# Your mac address 
-serverMACAddress = '00:04:3E:08:42:2F'
-port = 1
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.connect((serverMACAddress, port))
+s = serial.Serial( "/dev/rfcomm0", baudrate=38400 )
+s.write('STP31\r\n')
+s.write('ATSH1C0\r\n')
+s.write('STP31\r\n')
+s.write('ATSH1C0\r\n')
+s.write('STP31\r\n')
+s.write('ATSH1C0\r\n')
+s.write('STP31\r\n')
+s.write('ATSH1C0\r\n')
 ans=True
 while ans:
     print ("""
@@ -21,21 +25,13 @@ while ans:
     """)
     ans=raw_input("What would you like to do? ") 
     if ans=="1": 
-      s.send('STP31\r\n')
-      s.send('ATSH1C0\r\n')
-      s.send('69AA37901100\r\n')
+      s.write('69AA37901100\r\n')
     elif ans=="2":
-      s.send('STP31\r\n')
-      s.send('ATSH1C0\r\n')
-      s.send('6AAA37901100\r\n')
+      s.write('6AAA37901100\r\n')
     elif ans=="3":
-      s.send('STP31\r\n')
-      s.send('ATSH1C0\r\n')
-      s.send('24746C901100\r\n')
+      s.write('24746C901100\r\n')
     elif ans=="4":
-      s.send('STP31\r\n')
-      s.send('ATSH1C0\r\n')
-      s.send('21746C901100\r\n')
+      s.write('21746C901100\r\n')
     elif ans=="5":
       break 
     elif ans !="":
