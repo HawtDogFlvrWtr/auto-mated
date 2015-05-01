44d4fd0e67c14d653329ced7d6314dc5df82f68d
HawtDogFlvrWtr <hawtdogflvrwtr@gmail.com>
2015-05-01 14:29:37 -0400

    updated to push metrics to influx. Currently it's static.. need to work on capturing available pids and using them

--- uhacknect-metric.py
+++ uhacknect-metric.py
@@ -7,17 +7,29 @@
 
 import obd
 import time
+import urllib2
 
 #obd.debug.console = True
 
 def loopMetrics():
+    metricsArray = ["RPM","SPEED","TIMING_ADVANCE","INTAKE_TEMP","THROTTLE_POS","MAF","RUN_TIME","FUEL_LEVEL"]
+    metricsList = ','.join([str(x) for x in metricsArray])
     while 1:
-        # Loop until engine is shut off then wait
-        connection.watch(obd.commands.RPM)
-        connection.start()
-        connection.stop()
-        print connection.query(obd.commands.RPM)
-        time.sleep(5)
+        tempValues = []
+        for metrics in metricsArray:
+            # Loop until engine is shut off then wait
+            metricName = 'obd.commands.'+metrics
+            connection.watch(obd.commands[metrics])
+            connection.start()
+            connection.stop()
+            value = connection.query(obd.commands[metrics])
+            tempValues.append(value.value)
+        mainHost = "http://www.uhacknect.com/api/influxPush.php?vin=3C4PDCGG4FT641185&api=6054-4220-2524-3963&metric="
+        valuesList = ','.join([str(x) for x in tempValues])
+#        print "metrics List: "+metricsList
+#        print "values List: "+valuesList
+        output = urllib2.urlopen(mainHost+metricsList+"&values="+valuesList).read()
+        print output
 
 # Auto connect to obd device
 connection = obd.Async('/dev/pts/14')
@@ -28,12 +40,14 @@ while not connection.is_connected():
     connection = obd.Async('/dev/pts/14')
     time.sleep(5)
 print "Connected to device successfully"
+#supported = connection.supported_commands
+#print supported
 connection.watch(obd.commands.RPM)
 connection.start()
 connection.stop()
-checkEngineOn = connection.query(obd.commands.RPM)
 
 # Once connected, check if engine is running
+checkEngineOn = connection.query(obd.commands.RPM)
 while checkEngineOn.value is None:
     print "Engine is not started. Looping until the vehicle is turned on."
     time.sleep(5)
