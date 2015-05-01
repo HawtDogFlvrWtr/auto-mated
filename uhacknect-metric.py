70b21242537298e63cb8fb3170a4a2d496a525d2
HawtDogFlvrWtr <hawtdogflvrwtr@gmail.com>
2015-05-01 19:54:29 -0400

    adding exception handling and possible false outcomes

--- uhacknect-metric.py
+++ uhacknect-metric.py
@@ -11,7 +11,7 @@ import urllib2
 
 #obd.debug.console = True
 
-def loopMetrics():
+def loopMetrics(connection):
     metricsArray = ["RPM","SPEED","TIMING_ADVANCE","INTAKE_TEMP","THROTTLE_POS","MAF","RUN_TIME","FUEL_LEVEL"]
     metricsList = ','.join([str(x) for x in metricsArray])
     while 1:
@@ -26,31 +26,44 @@ def loopMetrics():
             tempValues.append(value.value)
         mainHost = "http://www.uhacknect.com/api/influxPush.php?vin=3C4PDCGG4FT641185&api=6054-4220-2524-3963&metric="
         valuesList = ','.join([str(x) for x in tempValues])
-#        print "metrics List: "+metricsList
-#        print "values List: "+valuesList
-        output = urllib2.urlopen(mainHost+metricsList+"&values="+valuesList).read()
+        # Check if we've been disconnected
+        if all(x is None for x in valuesList):
+            connection.unwatch_all()
+            kickOff()
+        print "metrics List: "+metricsList
+        print "values List: "+valuesList
+        # Attempt to push, loop over if no network connection
+        try:
+            output = urllib2.urlopen(mainHost+metricsList+"&values="+valuesList).read()
+        except:
+            loopMetrics(connection)
         print output
-        time.sleep(5)
+    time.sleep(5)
 
-# Auto connect to obd device
-connection = obd.Async()
 
-# Check if connected and continue, else loop
-while not connection.is_connected():
-    print "No valid device found. Please ensure ELM327 is on and connected. Looping with 5 seconds pause"
+def kickOff():
+    # Auto connect to obd device
     connection = obd.Async()
-    time.sleep(5)
-print "Connected to "+connection.get_port_name()+" successfully"
-#supported = connection.supported_commands
-#print supported
-connection.watch(obd.commands.RPM)
-connection.start()
-connection.stop()
+    # Check if connected and continue, else loop
+    while not connection.is_connected():
+        print "No valid device found. Please ensure ELM327 is on and connected. Looping with 5 seconds pause"
+        connection = obd.Async('/dev/pts/14')
+        time.sleep(5)
+    print "Connected to "+connection.get_port_name()+" successfully"
+    #supported = connection.supported_commands
+    #print supported
+    connection.watch(obd.commands.RPM)
+    connection.start()
+    connection.stop()
 
-# Once connected, check if engine is running
-checkEngineOn = connection.query(obd.commands.RPM)
-while checkEngineOn.value is None:
-    print "Engine is not started. Looping until the vehicle is turned on."
-    time.sleep(5)
-print "Engine is started.. doing my thing!"
-loopMetrics()
+    # Once connected, check if engine is running
+    checkEngineOn = connection.query(obd.commands.RPM)
+    while checkEngineOn.value is None:
+        print "Engine is not started. Looping until the vehicle is turned on."
+        time.sleep(5)
+    print "Engine is started.. doing my thing!"
+    try:
+        loopMetrics(connection)
+    except:
+        print "Caught It"
+kickOff()
