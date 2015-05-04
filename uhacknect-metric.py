8db71b9394bde38e6c1525e59f655ef400b924aa
HawtDogFlvrWtr <HawtDogFlvrWtr@gmail.com>
2015-05-03 21:26:23 -0400

    added more support for reading error codes from a vehicle. Moved functions up to make things better. Split checkEngineOn into it's own function and changed loop to run that if None values are found for rpm. Also have it write a final reord to influx of all zero's for data to drop all gauges and graphs to zero when the engine is turned off.

--- uhacknect-metric.py
+++ uhacknect-metric.py
@@ -12,68 +12,91 @@ import syslog
 
 #obd.debug.console = True
 
-def loopMetrics(connection):
+def obdQuery(connection,metric):
+    connection.watch(obd.commands[metric])
+    connection.start()
+    connection.stop()
+    
+
+def pushInflux(mainHost, metricsList, valuesList, connection):
+    # Attempt to push, loop over if no network connection
+    try:
+        output = urllib2.urlopen(mainHost+metricsList+"&values="+valuesList).read()
+    except:
+        # If we have no network connection, just loop through the entry until we get a connection again. FIX: NEED TO HAVE THIS WRITE TO A FILE AND UPLOAD WHEN AVAILABLE
+        mainLoop(connection)
+    syslog.syslog('Influxdb Web Return: '+output)
+    time.sleep(5)
+    
+
+def mainLoop(connection):
+    # FIX: NEED TO MAKE THIS HIS 01 TO DETERMINE SUPPORTED PID'S
     metricsArray = ["RPM","SPEED","TIMING_ADVANCE","INTAKE_TEMP","THROTTLE_POS","MAF","RUN_TIME","FUEL_LEVEL","COOLANT_TEMP","ENGINE_LOAD","FUEL_PRESSURE","INTAKE_PRESSURE","AMBIANT_AIR_TEMP","OIL_TEMP","FUEL_RATE"]
     metricsList = ','.join([str(x) for x in metricsArray])
     while 1:
         tempValues = []
         for metrics in metricsArray:
             # Loop until engine is shut off then wait
-            metricName = 'obd.commands.'+metrics
-            connection.watch(obd.commands[metrics])
-            connection.start()
-            connection.stop()
+            obdQuery(connection,metrics)
             value = connection.query(obd.commands[metrics])
             # Dump if RPM is none
             if metrics == 'RPM' and value.value is None:
                 connection.unwatch_all()
-                kickOff()
+                checkEngineOn(connection)
+                valuesList = "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+                # Push empty values so that gauges reset back to zero on uhacknect.com
+                pushInflux(mainHost, metricsList, valuesList, connection)
             tempValues.append(value.value)
         mainHost = "http://www.uhacknect.com/api/influxPush.php?vin=3C4PDCGG4FT641185&api=6054-4220-2524-3963&metric="
         valuesList = ','.join([str(x) for x in tempValues])
         # Check if we've been disconnected
         syslog.syslog('Influx Metrics List: '+metricsList)
         syslog.syslog('Influx Values List: '+valuesList)
-        # Attempt to push, loop over if no network connection
-        try:
-            output = urllib2.urlopen(mainHost+metricsList+"&values="+valuesList).read()
-        except:
-            loopMetrics(connection)
-        syslog.syslog('Influxdb Web Return: '+output)
-        time.sleep(5)
+        pushInflux(mainHost, metricsList, valuesList, connection)
 
 def checkCodes(connection):
     syslog.syslog('Checking engine for error codes...')
-    connection.watch(obd.commands.GET_DTC)
-    connection.start()
-    connection.stop()
-    errorCodes = connection.query(obd.commands.GET_DTC)
-    print errorCodes.value
+    obdQuery(connection,'STATUS')
+    errorCodes = connection.query(obd.commands.STATUS)
+    obdQuery(connection,'GET_DTC')
+    readCodes  = connection.query(obd.commands.GET_DTC)
+    print errorCodes
+    print readCodes
+
+def getVehicleInfo(connection):
+    syslog.syslog('Getting vehicle information')
+    obdQuery(connection,'GET_VIN')
+    vinNum = connection.query(obd.commands.GET_VIN)
+    print vinNum
+
+def checkEngineOn(connection):
+    # Once connected, check if engine is running
+    obdQuery(connection,'RPM')
+    checkEngineOn = connection.query(obd.commands.RPM)
+    while checkEngineOn.value is None:
+        syslog.syslog('Engine is not started. Looping until the vehicle is turned on.')
+        time.sleep(5)
+        obdQuery(connection,'RPM')
+        checkEngineOn = connection.query(obd.commands.RPM)
+    syslog.syslog('Engine is started..')
+ 
+    try:
+        mainLoop(connection)
+    except:
+        syslog.syslog('Caught escape key... exiting')
 
 def kickOff():
     # Auto connect to obd device
     syslog.syslog('Testing dev interface before real interface')
-    connection = obd.Async('/dev/pts/14')
+    connection = obd.Async('/dev/pts/2')
     # Check if connected and continue, else loop
     while not connection.is_connected():
         syslog.syslog('No valid device found. Please ensure ELM327 is on and connected. Looping with 5 seconds pause')
         connection = obd.Async()
         time.sleep(5)
     syslog.syslog('Connected to '+connection.get_port_name()+' successfully')
-    connection.watch(obd.commands.RPM)
-    connection.start()
-    connection.stop()
-
-    # Once connected, check if engine is running
-    checkEngineOn = connection.query(obd.commands.RPM)
-    while checkEngineOn.value is None:
-        syslog.syslog('Engine is not started. Looping until the vehicle is turned on.')
-        time.sleep(5)
-    syslog.syslog('Engine is started..')
+    getVehicleInfo(connection)
     checkCodes(connection)
- 
-    try:
-        loopMetrics(connection)
-    except:
-        syslog.syslog('Caught escape key... exiting')
+    checkEngineOn(connection)
+
 kickOff()
