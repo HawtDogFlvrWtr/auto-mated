92c8e33785848cbaacfb71fbaea4fd6c333fc21c
HawtDogFlvrWtr <hawtdogflvrwtr@gmail.com>
2015-09-02 09:41:59 -0400

    Initial lcd display file
    This will be the file needed to kick off the lcd update thread

---
+++ lcd.py
@@ -0,0 +1,42 @@
+sys.path.append('/usr/local/lib/lcd')
+from lcd import *
+
+def uDisplay():
+    while True:
+        queueSize = influxQueue.qsize()
+        timeNow = time.time()
+        # Setting up Engine text
+        if engineStatus == False:
+            engineText = "   Down"
+        else:
+            engineText = "     Up"
+  
+        # Setting up BT Text
+        if portName is None:
+            btStatus = "       Down"
+        else:
+            btStatus = "         Up"
+
+        # Setup debug
+        if debugOn == True:
+            debugMsg = " DEBUG"
+        else:
+            debugMsg = ""
+        if networkStatus == "    Up":
+            lcdDisplayText(0,0, "              ")
+            lcdDisplayText(0,0, "TIME: "+str(time.strftime("%I:%M:%S")))
+        else:
+            lcdDisplayText(0, 0, "Key:"+vehicleKey)
+        lcdDisplayText(0, 8, "BT:"+btStatus)
+        lcdDisplayText(0, 16, "ENGINE:"+engineText)
+        lcdDisplayText(0, 24, "NETWORK:"+networkStatus)
+        lcdDisplayText(0, 32, "METRICS:"+influxStatus)
+        lcdDisplayText(0, 40, "Q:"+str(queueSize)+ " S:"+str(metricsSuccess)+" "+debugMsg)
+        lcdDisplay()
+        time.sleep(1)
+
+
+# Kick off display thread
+displayThread = Thread(target=uDisplay)
+displayThread.setDaemon(True)
+displayThread.start()
