d5fcdf0d0b5b0bbae3161bde717b931c5708a0f6
root <hawtdogflvrwtr@gmail.com>
2017-11-09 02:27:12 +0000

    Added serial install option if you connect obd directly to gpio

---
+++ install/installserial.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat <<EOT >> /etc/motd
+   _____          __                      _____          __             .___
+  /  _  \  __ ___/  |_  ____             /     \ _____ _/  |_  ____   __| _/
+ /  /_\  \|  |  \   __\/  _ \   ______  /  \ /  \\\__  \\\   __\/ __ \ / __ | 
+/    |    \  |  /|  | (  <_> ) /_____/ /    Y    \/ __ \|  | \  ___// /_/ | 
+\____|__  /____/ |__|  \____/          \____|__  (____  /__|  \___  >____ | 
+        \/                                     \/     \/          \/     \/ 
+EOT
+
+sed -i -e '$i \/opt/auto-mated/automated-metric.py &\n' /etc/rc.local
+sudo apt-get install git-core -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+apt-get install --no-install-recommends htop python-dev python-pip -y
+cd /opt
+pip install obd psutil netifaces
